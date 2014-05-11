48470c2d31be7832018dc90ac0656e69d871dc4e
StreetStrider <teamfortresslife@gmail.com>
2014-05-12 01:09:32 +0400

    String: code/test improvements.

--- src/elements/String.js
+++ src/elements/String.js
@@ -5,22 +5,45 @@ module.exports = String;
 
 var
 	inherit = require('./../util/inherit'),
+	error   = require('./../util/error'),
 	Element = require('./Element');
 
-function String (string)
+function String (value)
 {
-	this.string = string;
+	this.value = value;
 }
 
 inherit(String, Element);
 
 String.prototype.toSQL = function String__toSQL ()
 {
-	var escaped = String.escape(this.string);
-	return "'"+ escaped +"'";
+	return String.stringy(this.value);
 };
+
+String.stringy = function String_stringy (value)
+{
+	if (typeof value !== 'string')
+	{
+		throw new String.NotAString({ value: value });
+	}
+
+	value = '' + value;
+	value = String.escape(value);
+	value = String.embrace(value);
+	return value;
+};
+
+String.NotAString = error.ErrorConstructor(
+	'Value is not a string.',
+	'not_a_string'
+);
 
 String.escape = function String_escape (string)
 {
 	return string.replace(/'/g, "''");
+};
+
+String.embrace = function String_embrace (string)
+{
+	return "'"+ string +"'";
 };
--- tests/src/elements/String.test.js
+++ tests/src/elements/String.test.js
@@ -6,6 +6,52 @@ var
 
 describe('String', function ()
 {
+	describe('embrace', function ()
+	{
+		it('embraces string in quotes', function ()
+		{
+			expect(String.embrace('abc')).toBe("'abc'");
+			expect(String.embrace('')).toBe("''");
+		});
+	});
+
+	describe('escape', function ()
+	{
+		it('escapes quotes', function ()
+		{
+			expect(String.escape("a'bc")).toBe("a''bc");
+			expect(String.escape('abc')).toBe('abc');
+			expect(String.escape('')).toBe('');
+		});
+	});
+
+	describe('stringy', function ()
+	{
+		it('outputs string', function ()
+		{
+			expect(String.stringy('abc')).toBe("'abc'");
+		});
+
+		it('escapes quotes', function ()
+		{
+			expect(String.stringy("a'; SELECT 1, ''; SELECT 2;")).toBe("'a''; SELECT 1, ''''; SELECT 2;'");
+		});
+
+		it('throws `NotAString` if another value', function ()
+		{
+			try
+			{
+				var value = 1;
+				String.stringy(value);
+			} catch (e)
+			{
+				expect(e).toEqual(String.NotAString({ value: value }));
+				return;
+			}
+			throw new Error;
+		});
+	});
+
 	describe('toSQL', function ()
 	{
 		it('outputs string', function ()
@@ -18,6 +64,21 @@ describe('String', function ()
 		{
 			var string = new String("a'; SELECT 1, ''; SELECT 2;");
 			expect(string.toSQL()).toBe("'a''; SELECT 1, ''''; SELECT 2;'");
-		})
+		});
+
+		it('throws `NotAString` if another value', function ()
+		{
+			try
+			{
+				var value = 1;
+				var string = new String(value);
+				string.toSQL();
+			} catch (e)
+			{
+				expect(e).toEqual(String.NotAString({ value: value }));
+				return;
+			}
+			throw new Error;
+		});
 	});
 });
